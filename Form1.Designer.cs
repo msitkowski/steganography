9db23bf8dcb80bc08ebcfccfba6ee3929dc710c9
msitkowski <mateuszsitek.sitkowski@gmail.com>
2017-10-21 23:46:52 +0200

    source files

---
+++ Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace techniki_steganografi_lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.loadFileButton = new System.Windows.Forms.Button();
+            this.insertButton = new System.Windows.Forms.Button();
+            this.readHiddenMsgButton = new System.Windows.Forms.Button();
+            this.saveModifiedFileButton = new System.Windows.Forms.Button();
+            this.msgTextBox = new System.Windows.Forms.TextBox();
+            this.msgLabel = new System.Windows.Forms.Label();
+            this.encryptionPasswordLbel = new System.Windows.Forms.Label();
+            this.steganograficKeyLabel = new System.Windows.Forms.Label();
+            this.encryptionPasswordTextBox = new System.Windows.Forms.TextBox();
+            this.steganographicKeyTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 480);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(673, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(640, 480);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // loadFileButton
+            // 
+            this.loadFileButton.Location = new System.Drawing.Point(13, 500);
+            this.loadFileButton.Name = "loadFileButton";
+            this.loadFileButton.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.loadFileButton.Size = new System.Drawing.Size(110, 37);
+            this.loadFileButton.TabIndex = 2;
+            this.loadFileButton.Text = "Wczytaj plik";
+            this.loadFileButton.UseVisualStyleBackColor = true;
+            this.loadFileButton.Click += new System.EventHandler(this.loadFileButton_Click);
+            // 
+            // insertButton
+            // 
+            this.insertButton.Location = new System.Drawing.Point(129, 500);
+            this.insertButton.Name = "insertButton";
+            this.insertButton.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.insertButton.Size = new System.Drawing.Size(110, 37);
+            this.insertButton.TabIndex = 3;
+            this.insertButton.Text = "Wstaw";
+            this.insertButton.UseVisualStyleBackColor = true;
+            this.insertButton.Click += new System.EventHandler(this.insertButton_Click);
+            // 
+            // readHiddenMsgButton
+            // 
+            this.readHiddenMsgButton.Location = new System.Drawing.Point(245, 500);
+            this.readHiddenMsgButton.Name = "readHiddenMsgButton";
+            this.readHiddenMsgButton.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.readHiddenMsgButton.Size = new System.Drawing.Size(110, 37);
+            this.readHiddenMsgButton.TabIndex = 4;
+            this.readHiddenMsgButton.Text = "Odczytaj ukrytą informację";
+            this.readHiddenMsgButton.UseVisualStyleBackColor = true;
+            this.readHiddenMsgButton.Click += new System.EventHandler(this.readHiddenMsgButton_Click);
+            // 
+            // saveModifiedFileButton
+            // 
+            this.saveModifiedFileButton.Location = new System.Drawing.Point(361, 499);
+            this.saveModifiedFileButton.Name = "saveModifiedFileButton";
+            this.saveModifiedFileButton.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.saveModifiedFileButton.Size = new System.Drawing.Size(110, 37);
+            this.saveModifiedFileButton.TabIndex = 5;
+            this.saveModifiedFileButton.Text = "Zapisz zmodyfikowany plik";
+            this.saveModifiedFileButton.UseVisualStyleBackColor = true;
+            this.saveModifiedFileButton.Click += new System.EventHandler(this.saveModifiedFileButton_Click);
+            // 
+            // msgTextBox
+            // 
+            this.msgTextBox.Location = new System.Drawing.Point(135, 542);
+            this.msgTextBox.Name = "msgTextBox";
+            this.msgTextBox.Size = new System.Drawing.Size(226, 20);
+            this.msgTextBox.TabIndex = 6;
+            // 
+            // msgLabel
+            // 
+            this.msgLabel.AutoSize = true;
+            this.msgLabel.Location = new System.Drawing.Point(13, 542);
+            this.msgLabel.Name = "msgLabel";
+            this.msgLabel.Size = new System.Drawing.Size(103, 13);
+            this.msgLabel.TabIndex = 7;
+            this.msgLabel.Text = "Tekst do osadzenia:";
+            // 
+            // encryptionPasswordLbel
+            // 
+            this.encryptionPasswordLbel.AutoSize = true;
+            this.encryptionPasswordLbel.Location = new System.Drawing.Point(16, 569);
+            this.encryptionPasswordLbel.Name = "encryptionPasswordLbel";
+            this.encryptionPasswordLbel.Size = new System.Drawing.Size(97, 13);
+            this.encryptionPasswordLbel.TabIndex = 8;
+            this.encryptionPasswordLbel.Text = "Hasło szyfrowania:";
+            // 
+            // steganograficKeyLabel
+            // 
+            this.steganograficKeyLabel.AutoSize = true;
+            this.steganograficKeyLabel.Location = new System.Drawing.Point(16, 595);
+            this.steganograficKeyLabel.Name = "steganograficKeyLabel";
+            this.steganograficKeyLabel.Size = new System.Drawing.Size(119, 13);
+            this.steganograficKeyLabel.TabIndex = 9;
+            this.steganograficKeyLabel.Text = "Klucz steganograficzny:";
+            // 
+            // encryptionPasswordTextBox
+            // 
+            this.encryptionPasswordTextBox.Location = new System.Drawing.Point(135, 569);
+            this.encryptionPasswordTextBox.Name = "encryptionPasswordTextBox";
+            this.encryptionPasswordTextBox.Size = new System.Drawing.Size(226, 20);
+            this.encryptionPasswordTextBox.TabIndex = 10;
+            // 
+            // steganographicKeyTextBox
+            // 
+            this.steganographicKeyTextBox.Location = new System.Drawing.Point(135, 595);
+            this.steganographicKeyTextBox.Name = "steganographicKeyTextBox";
+            this.steganographicKeyTextBox.Size = new System.Drawing.Size(226, 20);
+            this.steganographicKeyTextBox.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1325, 628);
+            this.Controls.Add(this.steganographicKeyTextBox);
+            this.Controls.Add(this.encryptionPasswordTextBox);
+            this.Controls.Add(this.steganograficKeyLabel);
+            this.Controls.Add(this.encryptionPasswordLbel);
+            this.Controls.Add(this.msgLabel);
+            this.Controls.Add(this.msgTextBox);
+            this.Controls.Add(this.saveModifiedFileButton);
+            this.Controls.Add(this.readHiddenMsgButton);
+            this.Controls.Add(this.insertButton);
+            this.Controls.Add(this.loadFileButton);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button loadFileButton;
+        private System.Windows.Forms.Button insertButton;
+        private System.Windows.Forms.Button readHiddenMsgButton;
+        private System.Windows.Forms.Button saveModifiedFileButton;
+        private System.Windows.Forms.TextBox msgTextBox;
+        private System.Windows.Forms.Label msgLabel;
+        private System.Windows.Forms.Label encryptionPasswordLbel;
+        private System.Windows.Forms.Label steganograficKeyLabel;
+        private System.Windows.Forms.TextBox encryptionPasswordTextBox;
+        private System.Windows.Forms.TextBox steganographicKeyTextBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace techniki_steganografi_lab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private byte[] EncryptMessage(string text, string passwd)
+        {
+            byte[] msg = null;
+            // setup message encryption
+            using (Aes encryptor = Aes.Create())
+            {
+                // hash password
+                SHA512 sha512 = new SHA512Managed();
+                byte[] hash = sha512.ComputeHash(Encoding.ASCII.GetBytes(passwd));
+                byte[] key = new byte[32];
+                byte[] iv = new byte[16];
+                Array.Copy(hash, key, 32);
+                Array.Copy(hash, iv, 16);
+                encryptor.Key = key;
+                encryptor.IV = iv;
+
+                // encrypt message
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(encryptor.Key, encryptor.IV), CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter sw = new StreamWriter(cs))
+                        {
+                            sw.Write(msgTextBox.Text);
+                        }
+                        msg = ms.ToArray();
+                    }
+                }
+            }
+            return msg;
+        }
+
+        private string DecryptMessage(byte[] msg, string passwd)
+        {
+            string res = null;
+
+            using (Aes decryptor = Aes.Create())
+            {
+                // hash password
+                SHA512 sha512 = new SHA512Managed();
+                byte[] hash = sha512.ComputeHash(Encoding.ASCII.GetBytes(passwd));
+                byte[] key = new byte[32];
+                byte[] iv = new byte[16];
+                Array.Copy(hash, key, 32);
+                Array.Copy(hash, iv, 16);
+                decryptor.Key = key;
+                decryptor.IV = iv;
+
+                using (MemoryStream ms = new MemoryStream(msg))
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, decryptor.CreateDecryptor(decryptor.Key, decryptor.IV), CryptoStreamMode.Read))
+                    {
+                        using (StreamReader sr = new StreamReader(cs))
+                        {
+                            res = sr.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return res;
+        }
+
+        private void HideMessageInBmp(Bitmap bmp, byte[] msg)
+        {
+            int msg_len_mask = 1 << 30;
+            int mask = 128;
+            int msg_iter = 0;
+            int next_msg_byte_counter = 0;
+            int px = 0;
+
+            // operations on image
+            for (int i = 0; i < bmp.Height; ++i)
+            {
+                for (int j = 0; j < bmp.Width; ++j)
+                {
+                    Color pixel = bmp.GetPixel(j, i);
+                    int red = pixel.R;
+                    int green = pixel.G;
+                    int blue = pixel.B;
+
+                    // get LSB
+                    int a1 = pixel.R & 1;
+                    int a2 = pixel.G & 1;
+                    int a3 = pixel.B & 1;
+
+                    int x1, x2;
+
+                    if (msg_len_mask > 0)
+                    {
+                        // get x1 and x2 bits value from msg.Length value
+                        x1 = ((msg.Length & msg_len_mask) == msg_len_mask) ? 1 : 0;
+                        msg_len_mask = msg_len_mask >> 1;
+                        x2 = ((msg.Length & msg_len_mask) == msg_len_mask) ? 1 : 0;
+                        msg_len_mask = msg_len_mask >> 1;
+                        px++;
+                    }
+                    else
+                    {
+                        // get x1 and x2 bits value from msg
+                        x1 = ((msg[msg_iter] & mask) == mask) ? 1 : 0;
+                        mask = mask >> 1;
+                        x2 = ((msg[msg_iter] & mask) == mask) ? 1 : 0;
+                        mask = mask >> 1;
+
+                        next_msg_byte_counter++;
+                        // when 4 pixels was encoded get next message byte
+                        // increment msg_iter, set mask to 128 and next_msg_byte_counter to 0
+                        if (next_msg_byte_counter == 4)
+                        {
+                            msg_iter++;
+                            mask = 128;
+                            next_msg_byte_counter = 0;
+                        }
+                    }
+
+                    // parity encoding
+                    if (x1 == (a1 ^ a3))
+                    {
+                        if (x2 != (a2 ^ a3))
+                        {
+                            // change a2
+                            green = (a2 == 1) ? pixel.G & 254 : pixel.G | 1;
+                        }
+                        // else change nothing
+                    }
+                    else
+                    {
+                        if (x2 == (a2 ^ a3))
+                        {
+                            // change a1
+                            red = (a1 == 1) ? pixel.R & 254 : pixel.R | 1;
+                        }
+                        else
+                        {
+                            // change a3
+                            blue = (a3 == 1) ? pixel.B & 254 : pixel.B | 1;
+                        }
+                    }
+                    // change pixel
+                    bmp.SetPixel(j, i, Color.FromArgb(red, green, blue));
+
+                    // break when all msg bytes are encrypted
+                    if (msg_iter >= msg.Length)
+                    {
+                        pictureBox2.Image = bmp;
+                        return;
+                    }
+                }
+            }
+        }
+
+        private byte[] ReadHiddenMessageFromBmp(Bitmap bmp)
+        {
+            byte[] msg = null;
+
+            int counter = 0;
+            int msg_len = 0;
+            int pixel_count = 0;
+            int decoded_bytes = 0;
+
+            // operations on image
+            for (int i = 0; i < bmp.Height; ++i)
+            {
+                for (int j = 0; j < bmp.Width; ++j)
+                {
+                    Color pixel = bmp.GetPixel(j, i);
+                    int red = pixel.R;
+                    int green = pixel.G;
+                    int blue = pixel.B;
+
+                    // get LSB
+                    int a1 = pixel.R & 1;
+                    int a2 = pixel.G & 1;
+                    int a3 = pixel.B & 1;
+
+                    int x1 = a1 ^ a3;
+                    int x2 = a2 ^ a3;
+
+                    // decoding msg length
+                    if (counter < 16)
+                    {
+                        msg_len |= x1;
+                        if (counter < 15)
+                        {
+                            msg_len <<= 1;
+                            msg_len |= x2;
+                            msg_len <<= 1;
+                        }
+                        counter++;
+                    }
+                    else
+                    {
+                        // initialize byte array for msg
+                        if (msg == null)
+                        {
+                            msg = new byte[msg_len];
+                            //for (int k = 0; k < msg.Length; ++k)
+                            //{
+                            //    msg[k] = 0;
+                            //}
+                        }
+
+                        int el = msg[decoded_bytes];
+                        el |= x1;
+                        el <<= 1;
+                        el |= x2;
+
+                        if (pixel_count < 3)
+                        {
+                            el <<= 1;
+                        }
+                        msg[decoded_bytes] = (byte)el;
+                        pixel_count++;
+
+                        // 4 pixels == 1 byte so read data to next msg array element
+                        if (pixel_count == 4)
+                        {
+                            pixel_count = 0;
+                            decoded_bytes++;
+                        }
+
+                        // check if all bytes decoded
+                        if (msg.Length == decoded_bytes)
+                        {
+                            return msg;
+                        }
+                    }
+                }
+            }
+            return msg;
+        }
+
+        private void loadFileButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog sourceDlg = new OpenFileDialog();
+            sourceDlg.Filter = "Bitmap (*.bmp)|*.bmp";
+
+            if (sourceDlg.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = Image.FromFile(sourceDlg.FileName);
+            }
+        }
+
+        private void insertButton_Click(object sender, EventArgs e)
+        {
+            // encrypt msg
+            byte[] msg = EncryptMessage(msgTextBox.Text, encryptionPasswordTextBox.Text);
+            HideMessageInBmp(new Bitmap(pictureBox1.Image), msg);
+        }
+        
+        private void readHiddenMsgButton_Click(object sender, EventArgs e)
+        {
+            byte[] encrypted_msg = ReadHiddenMessageFromBmp(new Bitmap(pictureBox2.Image));
+            string msg = DecryptMessage(encrypted_msg, encryptionPasswordTextBox.Text);
+            steganographicKeyTextBox.Text = msg;
+        }
+
+        private void saveModifiedFileButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveDlg = new SaveFileDialog();
+            saveDlg.Filter = "Bitmap (*.bmp)|*.bmp";
+
+            if (saveDlg.ShowDialog() == DialogResult.OK)
+            {
+                Bitmap image = new Bitmap(pictureBox2.Image);
+                image.Save(saveDlg.FileName, ImageFormat.Bmp);
+            }
+        }
+    }
+}
